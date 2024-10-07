f57111a5f90476e19570be8e029edf1c9cb1eb1b
BeerChaiyawat <73808398+BeerChaiyawat@users.noreply.github.com>
2024-10-07 15:34:13 +0700

    Create content.js

---
+++ content.js
@@ -0,0 +1,5 @@
+document.addEventListener("keydown", (event) => {
+    console.log(`Key pressed: ${event.key}`);
+    // คุณสามารถส่งข้อมูลไปยัง background script หรือเก็บข้อมูลในที่อื่นได้
+  });
+  
